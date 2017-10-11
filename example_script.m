e170e09e2b6bd4a6767ea02492313b6f669de183
Nazari <sna0240@str-mac-0378.str.corp>
2017-10-11 10:40:01 -0400

    First review of multilayer graph

--- example_script.m
+++ example_script.m
@@ -21,7 +21,7 @@
 % choices as in FIG 5 and FIG 8 of [1] for specific values of p and mu) 
 
 [A,S]=DirichletDCSBMBenchmark(n_nodes,n_layers,L,...
-'UpdateSteps',200,'theta',1,'communities',2,'q',1,...
+'UpdateSteps',200,'theta',1,'communities',com,'q',1,...
 'exponent',-2,'kmin',3,'kmax',150,'mu',mu,'maxreject',100);
 a1 = A{1};
 a2 = A{2};
@@ -39,19 +39,27 @@ save('L','L')
 save('S','S')
 save('p','p')
 save('mu','mu')
+save('n_layers','n_layers')
+save('n_nodes','n_nodes')
+save('com','com')
 cm1=['!scp A*.mat ma01-5200-0022:/disk1/maa/sam/random']
 cm2=['!scp mu.mat ma01-5200-0022:/disk1/maa/sam/random']
 cm3=['!scp p.mat ma01-5200-0022:/disk1/maa/sam/random']
 cm4=['!scp S.mat ma01-5200-0022:/disk1/maa/sam/random']
 cm5=['!scp L.mat ma01-5200-0022:/disk1/maa/sam/random']
+cm6=['!scp com.mat ma01-5200-0022:/disk1/maa/sam/random']
+cm7=['!scp n_nodes.mat ma01-5200-0022:/disk1/maa/sam/random']
+cm8=['!scp n_layers.mat ma01-5200-0022:/disk1/maa/sam/random']
 eval(cm1)
 eval(cm2)
 eval(cm3)
 eval(cm4)
 eval(cm5)
+eval(cm6)
+eval(cm7)
+eval(cm8)
 
 disp('Completed!')
-
 
 % EXAMPLE 2: TEMPORAL 
 % Temporal example with uniform interlayer dependencies between successive
--- initz.m
+++ initz.m
@@ -3,5 +3,6 @@ clear
 clc
 n_layers=4;
 n_nodes=500;
-p = 0.95; mu = 0.1;
+com = 5;
+p = 0.95; mu = 0.05;
 L = MultiplexDependencyMatrix(n_layers,p)
